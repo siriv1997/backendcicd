621b12858d8a9ab21cca3264e54530a3135bb569
KRKDKT029 <surendra@krify.net>
2022-09-20 12:42:17 +0530

    node

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+var app = express();
+
+app.get('/',(req,res) =>{
+    res.send({"mess":"I am working"})
+})
+
+app.listen(3000,()=>{
+    console.log('listening to 3000')
+})
